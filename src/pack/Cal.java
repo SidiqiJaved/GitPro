447eecea54a8e90682376e09ddcbfb3aea7c2700
SidiqiJaved <KJSidiqi@gmail.com>
2022-05-18 22:52:25 -0400

    4th commit from second package

---
+++ src/pack/Cal.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class Cal {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(" Calculate " );
+
+        int myNum =5;
+
+        System.out.println(myNum );
+    }
+}
